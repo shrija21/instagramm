69f957de2b6432c82cdafeac6794105780ce172b
shri <shrija.tumma1994@gmail.com>
2020-07-10 13:46:35 +0530

    Create new pics action

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,4 +3,23 @@ class PicsController < ApplicationController
 	def index
 		
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pics_params)
+
+		if @pic.save
+			redirect_to @pic
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def pics_params
+			params.require(:pic).permit(:title, :description)
+		end
 end
