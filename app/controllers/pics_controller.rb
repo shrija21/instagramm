835a16afd9f968d5fa9bdd6f3c9a36348e38fd38
shri <shrija.tumma1994@gmail.com>
2020-07-10 22:58:50 +0530

    Add devise and association with pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -9,11 +9,11 @@ class PicsController < ApplicationController
 	end
 
 	def new
-		@pic = Pic.new
+		@pic = current_user.pics.build
 	end
 
 	def create
-		@pic = Pic.new(pics_params)
+		@pic = current_user.pics.build(pics_params)
 
 		if @pic.save
 			redirect_to @pic, notice: "Yessss! It was posted!"
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200710054735) do
+ActiveRecord::Schema.define(version: 20200710171712) do
 
   create_table "pics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pics_on_user_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
